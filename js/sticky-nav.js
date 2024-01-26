ab0fb37b513f9e4f006cd9e90bc3f320e1da8880
alexpankov87 <deadwalkingday@gmail.com>
2024-01-26 13:56:12 +0300

    add changes

--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -4,4 +4,30 @@ $(window).scroll(function () {
     } else {
         $('.nav').removeClass('sticky')
     }
+    updateMenuStyles(); // Вызываем при загрузке страницы
+
+    $(window).scroll(function () {
+        updateMenuStyles();
+    });
+
+    $(window).resize(function () {
+        updateMenuStyles();
+    });
+
+    function updateMenuStyles() {
+        var scrollTop = $(window).scrollTop();
+        var windowWidth = $(window).width();
+        var $menu = $('.menu');
+
+        if (windowWidth <= 700) {
+            if (scrollTop > 100) {
+                $menu.css('margin-top', '89px');
+            } else {
+                $menu.css('margin-top', '209px');
+            }
+        } else {
+            $menu.css('margin-top', '');
+        }
+    }
+    
 });
