05d1fd1a732b1079bb31257267831859811951c4
alexpankov87 <deadwalkingday@gmail.com>
2024-01-27 13:27:32 +0300

    add

---
+++ js/formsender.js
@@ -0,0 +1,59 @@
+ function submitForm() {
+        let form = document.getElementById('feedback-form');
+
+        if (!form.checkValidity()) {
+
+            Swal.fire({
+                icon: "error",
+                title: "Ошибка",
+                text: "Пожалуйста, заполните все обязательные поля правильно.",
+                confirmButtonColor: '#f24822',
+                confirmButtonText: 'Окей'
+            });
+            return;
+        }
+
+        let formData = new FormData(form);
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", form.action, true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    let response = JSON.parse(xhr.responseText);
+                    if (response.status === "success") {
+                        // Успешный ответ от сервера, выполните дополнительные действия
+                        Swal.fire({
+                            icon: "success",
+                            title: "Успех",
+                            text: "Форма успешно отправлена!",
+                            confirmButtonColor: '#f24822',
+                            confirmButtonText: 'Окей'
+                        });
+                    } else {
+                        // Обработка ошибок
+                        Swal.fire({
+                            icon: "error",
+                            title: "Ошибка",
+                            text: "Ошибка при отправке формы: " + response.message,
+                            confirmButtonColor: '#f24822',
+                            confirmButtonText: 'Окей'
+                        });
+                    }
+                } else {
+                    // Обработка ошибок
+                    Swal.fire({
+                        icon: "error",
+                        title: "Ошибка",
+                        text: "Ошибка при отправке формы. Пожалуйста, попробуйте позже.",
+                        confirmButtonColor: '#f24822',
+                        confirmButtonText: 'Окей'
+                    });
+                }
+            }
+        };
+
+        xhr.send(new URLSearchParams(formData));
+    }
