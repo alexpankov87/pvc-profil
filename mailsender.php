1fae83eabc0d7a626aae2c3ea3b7cb6814f6fd42
alexpankov87 <deadwalkingday@gmail.com>
2024-02-09 12:29:13 +0300

    add

--- mailsender.php
+++ mailsender.php
@@ -19,6 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = "
         <html>
         <head>
+            <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'>
             <title>$subject</title>
         </head>
         <body>
@@ -26,16 +27,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <a href='https://pvc-profil.kz'>
                 <img class='logotype' src='https://pvc-profil.kz/css/img/logo.png' style='width:100px; height:90px; margin-left:20px; padding:10px; ' alt='Изображение логотипа' title='Компания Ампир'>
             </a>
-            <span style='font-weight: bold; color: #434343; font-size: 18px; margin-top: 30px;'>Завяка на обратную связь</span>
+            <span style='font-weight: bold; color: #434343; font-size: 18px; margin-top: 30px; font-family: Roboto, sans-serif;'>Завяка на обратную связь</span>
         </div>
         <div style='max-width: 100%; background-color: #c2c2c2; padding: 20px; border-radius: 5px;'>
-            <h3 style='color: #434343; text-decoration: underline;'>Клиент:</h3>
-            <p><strong>Имя:</strong> $name</p>
-            <p><strong>Телефон:</strong> $phone</p>
-            <p><strong>Отправлено со страницы:</strong> $source_page</p>
+            <h3 style='color: #434343; text-decoration: underline; font-family: Roboto, sans-serif;'>Клиент:</h3>
+            <p style='font-family: Roboto, sans-serif; font-weight: bold; color: #434343; font-size: 18px;'><strong>Имя:</strong> $name</p>
+            <p style='font-family: Roboto, sans-serif; font-weight: bold; color: #434343; font-size: 18px;'><strong>Телефон:</strong> $phone</p>
+            <p style='font-family: Roboto, sans-serif; font-weight: bold; color: #434343; font-size: 18px;'><strong>Отправлено со страницы:</strong> $source_page</p>
         </div>
         <div style='background: #ffd700; display: flex; max-width: 100%; padding-left: 160px; height:100px; border-radius: 5px;'>
-            <span style='font-weight: bold; color: #434343; font-size: 18px; display:flex; margin-top: 30px;'>Отправлено с сайта &nbsp; pvc-profil.kz</span>
+            <span style='font-weight: bold; color: #434343; font-size: 18px; display:flex; margin-top: 30px; font-family: Roboto, sans-serif;'>Отправлено с сайта &nbsp; pvc-profil.kz</span>
         </div>
         </body>
         </html>
