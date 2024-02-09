66b9751b8a2e822addc9dd0b8917007ff1753b36
alexpankov87 <deadwalkingday@gmail.com>
2024-02-09 12:21:09 +0300

    add

--- mailsender.php
+++ mailsender.php
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </head>
         <body>
         <div style='background: #ffd700; display:flex; max-width: 100%; padding-left: 160px; height:100px; border-radius: 5px;'>
-            <img class='logotype' src='https://pvc-profil.kz/css/img/logo.png' style='width:200px; height:200px; ' alt='Изображение логотипа' title='Компания Ампир'>
+            <img class='logotype' src='https://pvc-profil.kz/css/img/logo.png' style='width:80px; height:80px; ' alt='Изображение логотипа' title='Компания Ампир'>
             <span style='font-weight: bold; color: #434343; font-size: 18px; margin-top: 30px;'>Завяка на обратную связь</span>
         </div>
         <div style='max-width: 100%; background-color: #c2c2c2; padding: 20px; border-radius: 5px;'>
